3dfa72688658098d1c3a81575f5c0b3b54ad39dd
akanksha <akanksha.wrk@gmail.com>
2024-03-31 09:04:05 +0530

    update aes

--- AES.java
+++ AES.java
@@ -1,21 +1,21 @@
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
+import java.util.Scanner;
+
 public class AES {
+
     private static final String ALGORITHM = "AES";
     private static final byte[] keyValue =
-            new byte[] { 'T', 'h', 'i', 's', 'I', 's', 'A', 'S', 'e', 'c', 'r','e', 't', 'K', 'e', 'y' };
+            new byte[] { 'T', 'h', 'i', 's', 'I', 's', 'A', 'S', 'T', 'h', 'i', 's', 'I', 's', 'A', 'S'};
 
     public static String encrypt(String data) {
         try {
-            
             SecretKeySpec key = new SecretKeySpec(keyValue, ALGORITHM);
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, key);
             byte[] encryptedBytes = cipher.doFinal(data.getBytes());
             return Base64.getEncoder().encodeToString(encryptedBytes);
-
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -38,12 +38,35 @@ public class AES {
     }
 
     public static void main(String[] args) {
-        String data = "secret data";
-        String encryptedData = AES.encrypt(data);
-        String decryptedData = AES.decrypt(encryptedData);
-        System.out.println("Original Data: " + data);
-        System.out.println("Encrypted Data: " + encryptedData);
-        System.out.println("Decrypted Data: " + decryptedData);
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Enter:\n1. Sender's Side\n2. Receiver's Side\n3. Exit");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter data to encrypt:");
+                    String data = scanner.nextLine();
+                    String encryptedData = AES.encrypt(data);
+                    System.out.println("Encrypted Data: " + encryptedData);
+                    break;
+                case 2:
+                    System.out.println("Enter encrypted data to decrypt:");
+                    String encrypted = scanner.next();
+                    String decryptedData = AES.decrypt(encrypted);
+                    System.out.println("Decrypted Data: " + decryptedData);
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        } while (choice != 3);
     }
+
 }
 
