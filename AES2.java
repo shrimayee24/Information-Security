81cf2344b56fdd916be21cbd5bb3f2e6a0814413
akanksha <akanksha.wrk@gmail.com>
2024-03-31 21:33:39 +0530

    update

--- AES2.java
+++ AES2.java
@@ -8,40 +8,66 @@ public class AES2 {
             new byte[] {  'T', 'h', '6', 's', 'I', 's', 'h', 'S', 'e', 'c', 'r','e', 't', 'K', 'e', 'y' };
 
     public static String encrypt(String data) {
-        try {
+        try{
             SecretKeySpec key = new SecretKeySpec(keyValue, ALGORITHM);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            Cipher cipher= Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, key);
-            byte[] encryptedBytes = cipher.doFinal(data.getBytes());
-            return Base64.getEncoder().encodeToString(encryptedBytes);
-        } catch (Exception e) {
-            e.printStackTrace();
+            byte[] encryptedbytes=cipher.doFinal(data.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedbytes);
+        }
+        catch(Exception e){
+            System.out.println(e.getStackTrace());
             return null;
         }
+
     }
 
     public static String decrypt(String encryptedData) {
-        try {
+        try{
             SecretKeySpec key = new SecretKeySpec(keyValue, ALGORITHM);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            Cipher cipher= Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.DECRYPT_MODE, key);
-            byte[] decodedBytes = Base64.getDecoder().decode(encryptedData);
-            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
-            return new String(decryptedBytes);
+            byte[] decoded= Base64.getDecoder().decode(encryptedData);
+            byte[] decrypted=cipher.doFinal(decoded);
+            return new String(decrypted);
 
         }
-        catch (Exception e) {
-            e.printStackTrace();
+        catch(Exception e){
+            System.out.println(e.getStackTrace());
             return null;
         }
+
+
     }
 
     public static void main(String[] args) {
-        String data = "attack at 10 pm";
-        String encryptedData = AES2.encrypt(data);
-        String decryptedData = AES2.decrypt(encryptedData);
-        System.out.println("Original Data: " + data);
-        System.out.println("Encrypted Data: " + encryptedData);
-        System.out.println("Decrypted Data: " + decryptedData);
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Enter:\n1. Sender's Side\n2. Receiver's Side\n3. Exit");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter data to encrypt:");
+                    String data = scanner.nextLine();
+                    String encryptedData = AES.encrypt(data);
+                    System.out.println("Encrypted Data: " + encryptedData);
+                    break;
+                case 2:
+                    System.out.println("Enter encrypted data to decrypt:");
+                    String encrypted = scanner.next();
+                    String decryptedData = AES.decrypt(encrypted);
+                    System.out.println("Decrypted Data: " + decryptedData);
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        } while (choice != 3);
     }
 }
