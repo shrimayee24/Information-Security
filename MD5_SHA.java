17e42fddef705a35a3c122d258d7b26dc95c1f51
akanksha <akanksha.wrk@gmail.com>
2024-03-31 08:00:48 +0530

    update

---
+++ MD5_SHA.java
@@ -0,0 +1,25 @@
+import java.security.*;
+
+public class MD5_SHA{
+
+    public static void main(String[] args) {
+        String original = "Hello, MD5!";
+        String hash = getMD5(original);
+        System.out.println("MD5 Hash: " + hash);
+    }
+
+    public static String getMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA");
+            byte[] bytes = md.digest(input.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ RSA.java
@@ -0,0 +1,54 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+
+public class RSA {
+
+    public static void main(String[] args) throws Exception {
+
+        KeyPair keyPair = generateKeyPair();
+
+
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+
+
+        String originalMessage = "Hello, RSA!";
+        byte[] encryptedBytes = encrypt(originalMessage, publicKey);
+
+
+        String decryptedMessage = decrypt(encryptedBytes, privateKey);
+
+
+        System.out.println("Original Message: " + originalMessage);
+        System.out.println("Encrypted Message: " + Base64.getEncoder().encodeToString(encryptedBytes));
+        System.out.println("Decrypted Message: " + decryptedMessage);
+    }
+
+    // Function to generate RSA Key Pair
+    public static KeyPair generateKeyPair() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    // Function to encrypt message using RSA Public Key
+    public static byte[] encrypt(String message, PublicKey publicKey) throws Exception {
+        // Perform encryption
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(message.getBytes());
+    }
+
+    // Function to decrypt message using RSA Private Key
+    public static String decrypt(byte[] encryptedMessage, PrivateKey privateKey) throws Exception {
+        // Perform decryption
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return new String(cipher.doFinal(encryptedMessage));
+    }
+}
