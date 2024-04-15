9ebaf05e9fff0a6e3a717f75e4bad8ad485e3c24
akanksha <akanksha.wrk@gmail.com>
2024-04-15 14:58:00 +0530

    update

--- DiffieHillman.java
+++ DiffieHillman.java
@@ -1,57 +1,52 @@
-public class DiffieHillman
+import javax.crypto.KeyAgreement;
+import javax.crypto.spec.DHParameterSpec;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
-{
+public class DiffieHillman {
 
-    public static long side_A(long n,long g,long prA)
+    public static void main(String[] args) throws Exception {
+        // Generate Alice's key pair
+        KeyPairGenerator aliceKeyGen = KeyPairGenerator.getInstance("DH");
+        aliceKeyGen.initialize(2048);
+        KeyPair aliceKeyPair = aliceKeyGen.generateKeyPair();
+        PrivateKey alicePrivateKey = aliceKeyPair.getPrivate();
+        PublicKey alicePublicKey = aliceKeyPair.getPublic();
 
-    {
+        // Generate Bob's key pair using the same parameters
+        DHParameterSpec dhParams = ((javax.crypto.interfaces.DHPublicKey) alicePublicKey).getParams();
+        KeyPairGenerator bobKeyGen = KeyPairGenerator.getInstance("DH");
+        bobKeyGen.initialize(dhParams);
+        KeyPair bobKeyPair = bobKeyGen.generateKeyPair();
+        PrivateKey bobPrivateKey = bobKeyPair.getPrivate();
+        PublicKey bobPublicKey = bobKeyPair.getPublic();
 
-        long puA= ((long)Math.pow(g, prA))%n;
+        // Alice and Bob exchange public keys
+        KeyAgreement aliceKeyAgree = KeyAgreement.getInstance("DH");
+        aliceKeyAgree.init(alicePrivateKey);
+        aliceKeyAgree.doPhase(bobPublicKey, true);
 
-        return puA;
+        KeyAgreement bobKeyAgree = KeyAgreement.getInstance("DH");
+        bobKeyAgree.init(bobPrivateKey);
+        bobKeyAgree.doPhase(alicePublicKey, true);
 
+        // Generate the shared secret
+        byte[] aliceSharedSecret = aliceKeyAgree.generateSecret();
+        byte[] bobSharedSecret = bobKeyAgree.generateSecret();
+
+        // Display the shared secrets
+        System.out.println("Alice's shared secret: " + bytesToHex(aliceSharedSecret));
+        System.out.println("Bob's shared secret: " + bytesToHex(bobSharedSecret));
     }
 
-    public static long side_B(long n,long g, long prB)
-
-    {
-
-        long puB= ((long)Math.pow(g, prB))%n;
-
-        return puB;
-
+    // Convert bytes to hexadecimal string
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02X", b));
+        }
+        return result.toString();
     }
-
-    public static long sec_key(long n,long pr,long pu)
-
-    {
-
-        long k=((long)Math.pow(pu, pr))%n;
-
-        return k;
-
-    }
-
-    public static void main(String [] args)
-
-    {
-
-        long n = 11, g=3, prA=5, prB=8, puA, puB, k;
-
-        puA=side_A(n,g,prA);
-
-        puB=side_B(n,g,prB);
-
-        k=sec_key(n,prA,puB);
-
-        System.out.println("The public key of A is:" +puA);
-
-        System.out.println("The public key of B is:" +puB);
-
-        System.out.println("The shared secret key is:" +k);
-
-    }
-
-
-
 }
